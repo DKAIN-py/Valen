48b94dd836fa2b56716f9393cca9293458b04061
DKAIN-py <divyanshu06kain@gmail.com>
2026-09-12 19:59:40 +0000

    forward logic in Conv2D, multithreaded

--- include/Layers.hpp
+++ include/Layers.hpp
@@ -31,13 +31,15 @@ class Conv2D : public BaseForward{
         int in_channels;
         int out_channels;
         int stride{1};
-        int padding{0};
+        int pad_h{0}, pad_w{0};
 
-        Conv2D(std::vector<int> kernel_size, int in_channels, int out_channels, int stride, int padding, const Threadpool& pool_ref);
+        Conv2D(std::vector<int> kernel_size, int in_channels, int out_channels, int stride, int pad_h, int pad_w, const Threadpool& pool_ref);
 
         void load_parameters(const std::string& weights_path, const std::string& bias_path);
 
-        Nexus im2col(Nexus& input, int kh, int kw, int stride, int padding);
+        Nexus im2col(const Nexus& input, int kh, int kw, int stride, int pad_h, int pad_w);
+
+        Nexus pad2d(const Nexus& input, int pad_h, int pad_w);
 
         Nexus forward(const Nexus& input) override;
 };
--- src/Layers.cc
+++ src/Layers.cc
@@ -113,11 +113,12 @@ Nexus Linear::forward(const Nexus& input){
 
 // 2D Convlution
 
-Conv2D::Conv2D(std::vector<int> kernel_size, int in_channels, int out_channels, int stride, int padding, const Threadpool& pool_ref) : pool(pool_ref){
+Conv2D::Conv2D(std::vector<int> kernel_size, int in_channels, int out_channels, int stride, int pad_h, int pad_w, const Threadpool& pool_ref) : pool(pool_ref){
     this->kernel_size = kernel_size;
     this->in_channels = in_channels;
     this->stride = stride;
-    this->padding = padding;
+    this->pad_h = pad_h;
+    this->pad_w = pad_w;
     this->weights = Nexus({out_channels, in_channels*kernel_size[0]*kernel_size[1]});
     this->bias = Nexus({out_channels, 1});
 }
@@ -135,16 +136,42 @@ using Dynamic6DExtents = std::extents<int,
     std::dynamic_extent, // kh
     std::dynamic_extent>; // kw
 
-
-Nexus Conv2D::im2col(Nexus& input, int kh, int kw, int stride, int padding){
-    if (padding > 0){
-        // input.padding(padding);
-    }
-
+Nexus Conv2D::pad2d(const Nexus& input, int pad_h, int pad_w){
     int N = input.shape[0];
     int C = input.shape[1];
     int H = input.shape[2];
     int W = input.shape[3];
+
+    int H_pad = H+2*pad_h;
+    int W_pad = W+2*pad_w;
+
+    const float* src = input.data.data();
+
+    std::vector<float> dst(N*C*H_pad*W_pad, 0.0f);
+
+    for(int n{0}; n<N; n++){
+        for(int c{0}; c<C; c++){
+            for(int h{0}; h<H; h++){
+                const float* src_row = src +((n*C + c)*H + h)*W;
+                float* dst_row = dst.data() + (((n*C + c)*H_pad + (h+pad_h))*W_pad) + pad_w;
+
+                std::copy(src_row, src_row+W, dst_row);
+            }
+        }
+    }
+    Nexus padded_input({N,C,H_pad, W_pad});
+    padded_input.data = std::move(dst);
+    return padded_input;
+    
+}
+
+Nexus Conv2D::im2col(const Nexus& input, int kh, int kw, int stride, int pad_h, int pad_w){
+    Nexus padded_input = (pad_h>0 || pad_w>0) ? this->pad2d(input, pad_h, pad_w) : input;
+
+    int N = padded_input.shape[0];
+    int C = padded_input.shape[1];
+    int H = padded_input.shape[2];
+    int W = padded_input.shape[3];
 
     int H_out = (H-kh)/stride + 1;
     int W_out = (W-kw)/stride + 1;
@@ -166,7 +193,7 @@ Nexus Conv2D::im2col(Nexus& input, int kh, int kw, int stride, int padding){
     };
 
     std::layout_stride::mapping patches_map(patches_shape, patches_stride);
-    std::mdspan patches(input.data.data(), patches_map);
+    std::mdspan patches(padded_input.data.data(), patches_map);
 
     // transposed tensor 
     Dynamic6DExtents trans_shape(
@@ -188,7 +215,7 @@ Nexus Conv2D::im2col(Nexus& input, int kh, int kw, int stride, int padding){
     };
 
     std::layout_stride::mapping trans_map(trans_shape, trans_stride);
-    std::mdspan patches_transposed(input.data.data(), trans_map);
+    std::mdspan patches_transposed(padded_input.data.data(), trans_map);
 
     // reshpae to 2D matrix for GEMM
     int rows = C*kh*kw;
@@ -214,7 +241,69 @@ Nexus Conv2D::im2col(Nexus& input, int kh, int kw, int stride, int padding){
     }
 
     Nexus x_col({rows, cols});
-    x_col.data = x_col_data;
+    x_col.data = std::move(x_col_data);
 
     return x_col;
+}
+
+
+
+Nexus Conv2D::forward(const Nexus& input){
+    int N = input.shape[0];
+    int C = input.shape[1];
+    int H = input.shape[2];
+    int W = input.shape[3];
+    
+    int kh = this->kernel_size[0];
+    int kw = this->kernel_size[1];
+
+    int H_out = (H + 2*this->pad_h - kh)/this->stride + 1;
+    int W_out = (W + 2*this->pad_w - kw)/this->stride + 1;
+
+    Nexus x_col = this->im2col(input, kh, kw, this->stride, this->pad_h, this->pad_w);
+
+    Nexus output_mat({this->out_channels, N*H_out*W_out});
+
+    int effective_batch_size{N*H_out*W_out};
+
+    int threads = std::thread::hardware_concurrency();
+    if(threads==0) threads = 4;
+    int batches_per_thread = effective_batch_size/threads;
+
+    std::vector<float>* output_data_ptr = &output_mat.data;
+
+    std::latch barrier(threads);
+
+    int in_features = C*kh*kw;
+    int batch_cols = N*H_out*W_out;
+    int out_features = out_channels;
+
+    for(int i{0}; i<threads; i++){
+        int start = i*batches_per_thread;
+        int end = (i==threads-1) ? (effective_batch_size-1) : (start+batches_per_thread-1);
+
+        this->pool.enqueue_task([this, &x_col, &input, &output_mat, start, end, &barrier, out_features, in_features,batch_cols]{
+            const float* w_ptr = this->weights.data.data();
+            const float* x_ptr = x_col.data.data();
+            const float* b_ptr = this->bias.data.data();
+            float* out_ptr = output_mat.data.data();
+
+            for(int m{start}; m<=end; ++m){
+                for(int out{0}; out < out_features; ++out){
+                    float accumulator{0.0f};
+                    const float* w_row = w_ptr + (out*in_features);
+
+                    for(int in{0}; in<in_features; ++in){
+                        accumulator += w_row[in]*x_ptr[in * batch_cols + m];
+                    }
+
+                    out_ptr[out*batch_cols+m] = accumulator + b_ptr[out];
+                }
+            }
+
+            barrier.count_down();
+        });
+    }
+    
+    barrier.wait();
 }
